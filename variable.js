99fd284283fb9d0cfc30a43410872c9032426035
shamimspro <shamimspro@gmail.com>
2022-07-30 23:55:14 +0600

    fist commit

---
+++ variable.js
@@ -0,0 +1,31 @@
+// var x = 10;
+// console.log(x);
+// var y = 3;
+// console.log(x + y);
+// console.log(x - y)
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(typeof x);
+
+// var z = "5";
+// console.log(typeof z);
+// console.log(parseInt(z));
+
+// var a = 5.2;
+// console.log(typeof z);
+
+// var b = "5.5";
+// console.log(parseInt(b));
+// console.log(parseFloat(b));
+
+// console.log(a);
+// console.log("Sneha Mony");
+
+// var p = 0.1;
+// var q = 0.2;
+// var total = p + q;
+// total = total.toFixed(1);
+// total = parseFloat(total);
+// console.log(total);
+// console.log(typeof total);
