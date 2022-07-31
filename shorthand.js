8ec324f0c537626aa5aa094c7c2e6a8f0f7b3dbb
shamimspro <shamimspro@gmail.com>
2022-07-31 09:38:31 +0600

    shorthand

---
+++ shorthand.js
@@ -0,0 +1,29 @@
+var a = 5;
+// a = a + 10;
+a += 10; //This is shorthand
+console.log(a);
+
+var b = 10;
+// b = b - 5;
+b -= 5; //This is shorthand
+console.log(b);
+
+var c = 4;
+// c = c * 2;
+c *= 2; //This is shorthand
+console.log(c);
+
+var d = 20;
+// d = d / 2;
+d /= 2; //This is shorthand
+console.log(d);
+
+var e = 10;
+// e = e + 1;
+e++; //This is shorthand (Increment)
+console.log(e);
+
+var f = 10;
+// f = f - 1;
+f--; //This is shorthand (Decrement)
+console.log(f);
