3f961410411c99a9287214bc519443f7c7369796
shamimspro <shamimspro@gmail.com>
2022-08-01 23:12:32 +0600

    callback, for-loop, while-loop

---
+++ callback.js
@@ -0,0 +1,20 @@
+// function myFirst() {
+//     myDisplayer("Hello");
+// }
+
+// function mySecond() {
+//     myDisplayer("Goodbye");
+// }
+
+// myFirst();
+// mySecond();
+
+// function myDisplayer(some) {
+//     console.log(some);
+// }
+
+// var i = 1;
+// for (; i < 5; i++) {
+//     console.log(i);
+// }
+
---
+++ while-loop.js
@@ -0,0 +1,25 @@
+/*
+console.log("Md. Shamim Sarker");
+console.log("Md. Shamim Sarker");
+console.log("Md. Shamim Sarker");
+console.log("Md. Shamim Sarker");
+console.log("Md. Shamim Sarker");
+console.log("Md. Shamim Sarker");
+console.log("Md. Shamim Sarker");
+console.log("Md. Shamim Sarker");
+console.log("Md. Shamim Sarker");
+console.log("Md. Shamim Sarker");
+*/
+
+// var myName = 'Md. Shamim Sarker';
+// var num = 1;
+// while (num <= 5) {
+//     console.log(num + '.' + ' ' + myName);
+//     num++;
+// }
+
+var num = 1;
+while (num <= 10) {
+    console.log(num);
+    num += 2;
+}
